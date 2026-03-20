06e86765202acd26f7a4ba51417e349ee70215ca
Nikola <tau.taussn@gmail.com>
2026-03-20 12:46:28 +0100

    crash_fix6

--- backend/movix_app/lib/movix_app_web/controllers/api/movies_controller.ex
+++ backend/movix_app/lib/movix_app_web/controllers/api/movies_controller.ex
@@ -45,8 +45,8 @@ defmodule MovixAppWeb.Api.MoviesController do
     end
   end
 
-  defp upload_to_cloudinary(slika) do
-    case Cloudex.upload(slika, %{folder: "movies"}) do
+  defp upload_to_cloudinary(%Plug.Upload{path: path}) do
+    case Cloudex.upload(path, %{folder: "movies"}) do
       {:ok, result} -> {:ok, result.secure_url}
       {:error, err} -> {:error, err}
     end
